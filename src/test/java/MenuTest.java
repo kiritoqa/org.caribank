0c25332e3b732e2b9891c4cef72c934effc614be
kiritoqa <khnayt@gmail.com>
2020-10-06 09:15:47 +0300

    test Jenkins

--- src/test/java/MenuTest.java
+++ src/test/java/MenuTest.java
@@ -68,5 +68,9 @@ public class MenuTest {
         Assert.assertEquals(8, menuPage.getNumberOfFooterCategories());
     }
 
+    @Test
+    public void footerMenuSize2(){
+        Assert.assertEquals(7, menuPage.getNumberOfFooterCategories());
+    }
 }
 
